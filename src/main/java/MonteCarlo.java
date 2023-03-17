d9445836fca325334082f9f6c5d3381f69577b5f
ahbeym <101992409+ahbeym@users.noreply.github.com>
2023-03-17 18:48:19 -0400

    Fixed some errors, still negatives somehow

--- src/main/java/MonteCarlo.java
+++ src/main/java/MonteCarlo.java
@@ -1,22 +1,18 @@
 public class MonteCarlo {
 
-    static int x = 1000;
-    static int a = 24693; //multiplier
-    static int c = 3517; //increment
-    static int k = 2^17; //modulus
-    static double u = 0; //quotient of x/K
-    static double v1 = 0;
+    static int x_0 = 1000;
+    static final int A = 24693; //multiplier
+    static final int C = 3517; //increment
+    static final int K = (int) Math.pow(2,17); //modulus
+
     public static double [] randomNumberGenerator(){
         //need nested?
         double [] nums = new double[500];
-        nums[0] = x;
-//        for (int i = 1; i < 500; i++){
-//            nums[i] = ( (a*nums[i-1]) + c) % k;
-//        }
-        for (int i = 1; i < 500; i++){
-            v1 = ( (a*nums[i-1]) + c) % k;
-            u = v1 /k;
-            nums[i] = u;
+        int x_i = x_0;
+        for (int i = 0; i < 500; i++){
+            x_i = ((x_i*A)+C) % K;
+            double u_i = (double) x_i / K;
+            nums[i] = u_i;
         }
         return nums;
     }

1e9b8556192f866fc68cf351b44bb7565fb305e8
ahbeym <101992409+ahbeym@users.noreply.github.com>
2023-03-17 18:37:54 -0400

    fixed errors

--- src/main/java/MonteCarlo.java
+++ src/main/java/MonteCarlo.java
@@ -1,6 +1,6 @@
 public class MonteCarlo {
 
-    static int x;
+    static int x = 1000;
     static int a = 24693; //multiplier
     static int c = 3517; //increment
     static int k = 2^17; //modulus
@@ -9,16 +9,16 @@ public class MonteCarlo {
     public static double [] randomNumberGenerator(){
         //need nested?
         double [] nums = new double[500];
-//        nums[0] = x;
-////        for (int i = 1; i < 500; i++){
-////            nums[i] = ( (a*nums[i-1]) + c) % k;
-////        }
+        nums[0] = x;
 //        for (int i = 1; i < 500; i++){
-//            v1 = ( (a*nums[i-1]) + c) % k;
-//            u = v1 /k;
-//            nums[i] = u;
+//            nums[i] = ( (a*nums[i-1]) + c) % k;
 //        }
-//        return nums;
+        for (int i = 1; i < 500; i++){
+            v1 = ( (a*nums[i-1]) + c) % k;
+            u = v1 /k;
+            nums[i] = u;
+        }
+        return nums;
     }
     public static void main(String[] args){
         double [] numList;

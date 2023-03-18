c47113845413e9abfb12d442ad82433548e94d69
timothy <und2yw@virginia.edu>
2023-03-17 22:32:10 -0400

    finished

--- src/main/java/MonteCarlo.java
+++ src/main/java/MonteCarlo.java
@@ -1,26 +1,28 @@
+import java.util.Arrays;
+
 public class MonteCarlo {
 
     static final int x_0 = 1000;
     static final int A = 24693; //multiplier
     static final int C = 3517; //increment
     static final int K = (int) Math.pow(2,17); //modulus
-    static final int num_of_RV = 500;
-    static double [] W = new double[num_of_RV];
+    static final int num_of_RV = 2003;
+    static double [] W = new double[500];
     static int iter = -1;
     static double [] x = new double[num_of_RV];
     static double [] u = new double[num_of_RV];
 
-    public static double [] randomNumberGenerator(){
+    public static double [] randomNumberGenerator() {
        x[0] = ((A* x_0+ C )% K);
        u[0] = x[0] / (double) K;
-       for (int i = 1; i < num_of_RV; i++){
+       for (int i = 1; i < num_of_RV; i++) {
            x[i] = (( A* x[i-1]  + C) % K);
            u[i] = x[i] / (double) K;
        }
        return u;
     }
     public static double[] availabilityAndTimeChecker() {
-        for (int i = 0; i < num_of_RV; i++) {
+        for (int i = 0; i < 500; i++) {
             int calls = 0;
             while (calls < 4) {
                 calls++;
@@ -47,14 +49,14 @@ public class MonteCarlo {
         }
         return W;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double [] numList;
         double [] timeList;
         numList = randomNumberGenerator();
         timeList = availabilityAndTimeChecker();
-        for(int i = 0; i < numList.length; i++){
-            System.out.println("Element " + i + ": " + numList[i] + ", " + timeList[i]);
+        Arrays.sort(timeList);
+        for(int i = 0; i < 500; i++) {
+            System.out.println("W: " + timeList[i]);
         }
-
     }
 }

2befa0c2a4bed0bc411fb43583965078043c2bb7
ahbeym <101992409+ahbeym@users.noreply.github.com>
2023-03-19 13:03:39 -0400

    pog

--- src/main/java/MonteCarlo.java
+++ src/main/java/MonteCarlo.java
@@ -50,15 +50,53 @@ public class MonteCarlo {
         }
         return W;
     }
+    public static double calcMean(double [] a){
+        double sum = 0;
+        for (double d: a){
+            sum += d;
+        }
+        return sum / a.length;
 
+    }
+    public static double calcMedian(double [] a){
+        Arrays.sort(a);
+        double median;
+        if (a.length % 2 == 0)
+            median = ((double)a[a.length/2] + (double)a[a.length/2 - 1])/2;
+        else
+            median = (double) a[a.length/2];
+        return median;
+    }
+    public static double round(double number, int decimals) {
+        double out = number * Math.pow(10, decimals);
+        out = Math.round(out) / Math.pow(10, decimals);
+
+        return out;
+    }
+    public static double[] round(double[] array, int decimals) {
+        double[] out = new double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            out[i] = round(array[i], decimals);
+        }
+
+        return out;
+    }
     public static void main(String[] args) {
         double [] numList;
         double [] timeList;
         numList = randomNumberGenerator();
         timeList = availabilityAndTimeChecker();
         Arrays.sort(timeList);
+        double mean = calcMean(timeList);
+        double median = calcMedian(timeList);
+        double [] neat = round(timeList, 4);
+        System.out.println("W mean: " + mean);
+        System.out.println("W median: " + median);
+        System.out.println("----------------------------");
         for(int i = 0; i < 500; i++) {
-            System.out.println("W: " + timeList[i]);
+            int form = i+1;
+
+            System.out.println("Value " + form + ": " + neat[i]);
         }
     }
 }
